b392c169e4cf6d3a7f64700b7a375f033be0e7c5
Eren Livingstone <eren.livingstone@gmail.com>
2021-04-11 00:12:07 -0400

    Add User entity, update db schema, create new db migration. Create helper npm script command for creating migrations

--- server/src/mikro-orm.config.ts
+++ server/src/mikro-orm.config.ts
@@ -2,6 +2,7 @@ import { MikroORM } from "@mikro-orm/core";
 import path from "path";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
+import { User } from "./entities/User";
 
 export default {
     dbName: 'el-reddit',
@@ -9,7 +10,7 @@ export default {
     password: 'whoisthisman',
     type: 'postgresql',
     debug: !__prod__,
-    entities: [Post],
+    entities: [Post, User],
     migrations: {
         path: path.join(__dirname, './migrations'),
         pattern: /^[\w-]+\d+\.[tj]s$/
